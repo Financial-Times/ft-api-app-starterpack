54bffa3a07e05a7991ccc9d5e9cee5bb0ee9d7c2
Tomasz Libich <tomasz.libich@ft.com>
2016-05-16 08:53:28 +0100

    Updating client build task

--- build/tasks/client-build.js
+++ build/tasks/client-build.js
@@ -3,7 +3,7 @@
     const runSequence = require('run-sequence');
 
     module.exports = function (callback) {
-        return runSequence('client-build-system', 'client-build-html', 'client-copy-origami-bundle', callback);
+        return runSequence('client-build-system', 'client-build-html', 'client-copy-origami-bundle', 'client-sass', callback);
     };
 
     module.exports.dependencies = ['clean'];
